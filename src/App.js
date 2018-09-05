b7768056f0e5e9380b964ca7661a09b14812ebb1
ronakruparel <rr9323@gmail.com>
2018-09-05 15:13:28 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import Date from './components/Date'
+class App extends Component {
+  render() {
+    return (
+      <Date />
+    );
+  }
+}
+
+export default App;
